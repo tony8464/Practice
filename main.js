9843adac958382197a0b6c5223a546fd3e8e6504
tony8464 <46433974+tony8464@users.noreply.github.com>
2019-10-22 22:15:18 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const leftButton = document.getElementById("lButton")
+const rightButton = document.getElementById("rButton")
+const ltxt = document.getElementById("ltxt")
+const rtxt = document.getElementById("rtxt");
+const center = document.getElementById("center")
+const mirror = document.getElementById('mirror')
+
+let lastMessage
+
+// addMessage('hi', 'left')
+function addMessage(text, sender){ 
+  if(text === '') {
+    return
+  }
+  const newLi = document.createElement("div")
+  newLi.textContent = text
+  newLi.className = sender + 'Message'
+  function showAlert() {
+    // center.removeChild(newLi)
+    newLi.innerHTML = "123"
+    center.removeChild(lastMessage)
+  }
+  newLi.addEventListener('click', showAlert)
+  center.appendChild(newLi);
+  lastMessage = newLi
+}
+
+function newLmessage() {
+   // newLi.appendChild(text)
+    addMessage(ltxt.value, 'left')
+    ltxt.value = ''
+}
+
+function newRmessage() {
+  const newLi = document.createElement("div");
+  const rtext = document.createTextNode(rtxt.value)
+
+  newLi.className = "rightMessage";
+  newLi.appendChild(rtext)
+  document.getElementById("center").appendChild(newLi)
+  
+}
+
+function updateMirror(event) {
+  mirror.textContent = 'last pressed keyCode: ' + event.keyCode
+  if (event.keyCode === 90) {
+    // do something
+  }
+}
+
+ltxt.addEventListener('keyup', updateMirror)
+
+leftButton.addEventListener('click', newLmessage)
+rightButton.addEventListener('click', newRmessage)
