87ce8c8b348467f6b8a833c3afe3ec801cebddb8
tony8464 <46433974+tony8464@users.noreply.github.com>
2019-10-26 01:15:12 -0500

    remake

--- main.js
+++ main.js
@@ -1,51 +1,62 @@
-const leftButton = document.getElementById("lButton")
-const rightButton = document.getElementById("rButton")
-const ltxt = document.getElementById("ltxt")
-const rtxt = document.getElementById("rtxt");
+const leftButton = document.getElementById("leftButton")
+const rightButton = document.getElementById("rightButton")
+const leftTextInput = document.getElementById("leftTextInput")
+const rightTextInput = document.getElementById("rightTextInput");
 const center = document.getElementById("center")
 const mirror = document.getElementById('mirror')
-
-let lastMessage
 
 function addMessage(text, sender){ 
   if(text === '') {
     return
   }
-  const newLi = document.createElement("div")
-  newLi.textContent = text
-  newLi.className = sender + 'Message'
-  function showAlert() {
-    newLi.innerHTML = "123"
-    center.removeChild(lastMessage)
-  }
-  newLi.addEventListener('click', showAlert)
-  center.appendChild(newLi);
-  lastMessage = newLi
+  const MessageBox = document.createElement("div");
+  const DelButton = document.createElement("div");
+  const MessageText = document.createElement("div");  
+  const TimeStamp = document.createElement('div');
+
+  MessageBox.className = sender + "MessageBox"
+  DelButton.className = sender + "DelButton"
+  MessageText.className = sender + "MessageText"
+  TimeStamp.className = sender + "TimeStamp"
+
+  MessageText.textContent = text
+  DelButton.textContent = "🗑️"
+  TimeStamp.textContent = "10/26/19 9:04am"
+
+  MessageBox.appendChild(DelButton)
+  MessageBox.appendChild(MessageText)
+  MessageBox.appendChild(TimeStamp)
+
+  center.appendChild(MessageBox); 
+
+  DelButton.addEventListener('click', removeMessage)
+  function removeMessage(){
+    center.removeChild(MessageBox)
+}
+}
+function newLeftmessage() {
+    addMessage(leftTextInput.value, 'left')
+    leftTextInput.value = '';
+}
+function newRightmessage(){  
+  addMessage(rightTextInput.value, 'right')
+  rightTextInput.value = '';
 }
 
-function newLmessage() {
-    addMessage(ltxt.value, 'left')
-    ltxt.value = ''
-}
-
-function newRmessage() {
-  const newLi = document.createElement("div");
-  const rtext = document.createTextNode(rtxt.value)
-
-  newLi.className = "rightMessage";
-  newLi.appendChild(rtext)
-  document.getElementById("center").appendChild(newLi)
-  
-}
-
-function updateMirror(event) {
+function enterLeft(event) {
   mirror.textContent = 'last pressed keyCode: ' + event.keyCode
-  if (event.keyCode === 90) {
-    // do something
+  if (event.keyCode === 13) {
+    NewLeftmessage() 
+  }
+}
+function enterRight (event){
+  if (event.keyCode ===13) {
+    newRightmessage()
   }
 }
 
-ltxt.addEventListener('keyup', updateMirror)
+leftTextInput.addEventListener('keyup', enterLeft)
+rightTextInput.addEventListener('keyup', enterRight)
 
-leftButton.addEventListener('click', newLmessage)
-rightButton.addEventListener('click', newRmessage)
+leftButton.addEventListener('click', newLeftmessage);
+rightButton.addEventListener('click', newRightmessage)
